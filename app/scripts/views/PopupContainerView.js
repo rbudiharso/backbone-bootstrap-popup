68b79a62d185da332f14489f0440a8e81e86b194
Rahmat Budiharso <rbudiharso@gmail.com>
2014-02-03 16:42:34 +0700

    fix popup
    - add class fade
    - remove manual css setting

--- app/scripts/views/PopupContainerView.js
+++ app/scripts/views/PopupContainerView.js
@@ -4,29 +4,22 @@
     Blog.Views.PopupContainerView = Backbone.View.extend({
 
         template: JST['app/scripts/templates/PopupContainerView.ejs'],
-        events: {
-            'click .close': 'hidePopup'
-        },
+        className: 'modal fade',
         defaults: {
             title: 'Popup'
         },
         render: function() {
+            var self = this;
+
             this.$el.html(this.template({popup: this.defaults}));
             this.$el.modal({show: false});
-            this.$el.css({
-                top: '30px',
-                left: 0,
-                width: '100%',
-                zIndex: 9999
-            });
             $('body').append(this.el);
             this.appendMainView()
             this.$el.modal('show');
+            this.on('hidden.bs.modal', function() {
+                self.remove();
+            });
             return this;
-        },
-        hidePopup: function() {
-            this.$el.modal('hide');
-            this.remove();
         },
         renderMainView: function() {
             return {el: 'Override this to render main view'};
